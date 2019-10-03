d53f5a0b7c41e73de61b92ab79942f274142477c
Gastón Tognola <tognola08@gmail.com>
2019-10-03 00:28:53 -0300

    Eliminado el print de dat

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function that returns a list of functions to:
+  ## 1. set the matrix
+  ## 2. get the matrix
+  ## 3. set the inverse of the matrix
+  ## 4. get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The function looks for the inverse of X in the cache, if it stored
+## it doesn't calculate it again, or if it isn't, it calculates the inverse of x
+## and returns it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  print(data)
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
